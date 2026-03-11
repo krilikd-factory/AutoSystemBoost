c49278a9f1289e9e6f68588db8bc1e2051bb7039
Dima Krylov <krilikd@gmail.com>
2026-03-11 20:44:50 +0100

    Update version in action.sh to V19.1

--- action.sh
+++ action.sh
@@ -16,7 +16,7 @@ if [ -z "$MMRL" ] && [ ! -z "$MAGISKTMP" ]; then
 fi
 
 PROFILE="$(cat "$MODDIR/current_profile" 2>/dev/null || echo balanced)"
-echo "- AutoSystemBoost V19 KR-4 KR-3 Final"
+echo "- AutoSystemBoost V19.1"
 echo "- Current profile: $PROFILE"
 echo ""
 echo "- Install KSUWebUIStandalone or WebUI X"
