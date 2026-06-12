ea71bfbe3934eb8e7302b022666663ba5230255d
Dima Krylov <krilikd@gmail.com>
2026-06-12 10:45:10 +0200

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -6,13 +6,7 @@ REPLACE="
 "
 
 set_permissions() {
-  set_perm_recursive "$MODPATH" 0 0 0755 0644
-  for _f in "$MODPATH"/*.sh "$MODPATH"/bin/* "$MODPATH"/tools/*.sh "$MODPATH"/tools/*.py \
-            "$MODPATH"/tools/logkit/*.sh "$MODPATH"/runtime/*.sh "$MODPATH"/profiles/*.sh; do
-    [ -e "$_f" ] && set_perm "$_f" 0 0 0755
-  done
-  [ -d "$MODPATH/system/vendor" ] && \
-    set_perm_recursive "$MODPATH/system/vendor" 0 0 0755 0644 u:object_r:vendor_file:s0
+  :
 }
 
 SKIPUNZIP=1
