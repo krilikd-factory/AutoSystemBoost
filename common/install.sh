bde4d8abb63bf762e6128e5898077da153826dcc
Dima Krylov <krilikd@gmail.com>
2026-07-30 22:27:33 +0200

    Add files via upload

--- common/install.sh
+++ common/install.sh
@@ -3580,6 +3580,7 @@ AutoSystemBoost' $APIOCXM
 	    ! -name "asb_analyze.py" \
 	    ! -name "asb_camera_repair.sh" \
 	    ! -name "asb_sysui_watch.sh" \
+	    ! -name "asb_diag.sh" \
 	    -delete 2>/dev/null
 	  chmod 0755 "$MODPATH/tools/asb_camera_repair.sh" 2>/dev/null || true
 	  chmod 0755 "$MODPATH/tools/asb_sysui_watch.sh" 2>/dev/null || true
